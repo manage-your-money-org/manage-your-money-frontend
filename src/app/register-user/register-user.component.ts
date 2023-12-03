e7ded772b89e3a2fe3d9a5aa72bc6c12b0409a1d
Rohit <rkumar8092378845@gmail.com>
2023-12-04 00:16:00 +0530

    - customised register user component design
    #1

--- src/app/register-user/register-user.component.ts
+++ src/app/register-user/register-user.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {NgForm} from "@angular/forms";
 import {MymUtil} from "../shared/util/MymUtil";
 import {Router} from "@angular/router";
 import {UserAuthenticationService} from "../services/user-authentication/user-authentication.service";
@@ -23,94 +22,101 @@ export class RegisterUserComponent implements OnInit {
   isRegistrationSuccessful = false;
   successMessage: string = ''
 
+  hide = true;
+
   constructor(private router: Router, private userAuthenticationService: UserAuthenticationService) {
   }
 
   ngOnInit(): void {
   }
 
-  registerUser(registerForm: NgForm) {
+  registerUser() {
 
     // console.log(registerForm);
 
     console.log(`${this.name}, ${this.emailId}, ${this.password}, ${this.confirmPassword}`);
 
+    if (this.checkIfFormValid()) {
+
+      let userRequest: UserRequest = {
+        name: this.name.trim(),
+        emailId: this.emailId.trim(),
+        password: this.password.trim()
+      }
+
+      this.userAuthenticationService.registerUser(userRequest).subscribe({
+
+        next: response => {
+
+          let status = response.status;
+
+          console.log("Status: " + status);
+
+          if (status == 201) {
+            this.isRegistrationSuccessful = true;
+            this.successMessage = response.body.message;
+          } else {
+
+            this.isRegistrationSuccessful = false;
+            this.successMessage = '';
+            this.isFormValid = false;
+            this.invalidFormMessage = response.body.message;
+          }
+        },
+
+        error: err => {
+
+          let status = err.status;
+
+          console.log("Status: " + status);
+          this.isFormValid = false;
+          this.invalidFormMessage = err.error.message;
+        }
+
+      })
+    }
+
+
+  }
+
+  checkIfFormValid(): boolean {
+
     if (!MymUtil.isStringValid(this.name)) {
       this.isFormValid = false;
       this.invalidFormMessage = "Please enter a valid name";
-      return;
+      return false;
     }
 
     if (!MymUtil.isEmailIdValid(this.emailId.trim())) {
 
       this.isFormValid = false;
       this.invalidFormMessage = 'Please enter a valid email id';
-      return;
+      return false;
     }
 
     if (!MymUtil.isStringValid(this.password)) {
 
       this.isFormValid = false;
       this.invalidFormMessage = "Please enter a password";
-      return;
+      return false;
     }
 
     if (!MymUtil.isPasswordValid(this.password)) {
 
       this.isFormValid = false;
       this.invalidFormMessage = "Password should be at-lease 8 characters long and should contain a special character (!@#$%^&*)";
-      return;
+      return false;
     }
 
     if (this.password !== this.confirmPassword) {
       this.isFormValid = false;
       this.invalidFormMessage = 'Password does not match';
-      return;
+      return false;
     }
 
     this.isFormValid = true;
     this.invalidFormMessage = '';
-
-    let userRequest: UserRequest = {
-      name: this.name.trim(),
-      emailId: this.emailId.trim(),
-      password: this.password.trim()
-    }
-
-    this.userAuthenticationService.registerUser(userRequest).subscribe({
-
-      next: response => {
-
-        let status = response.status;
-
-        console.log("Status: " + status);
-
-        if (status == 201) {
-          this.isRegistrationSuccessful = true;
-          this.successMessage = response.body.message;
-        } else {
-
-          this.isRegistrationSuccessful = false;
-          this.successMessage = '';
-
-          this.isFormValid = false;
-          this.invalidFormMessage = response.body.message;
-        }
-
-        //this.router.navigate(["login"])
-      },
-
-      error: err => {
-
-        let status = err.status;
-
-        console.log("Status: " + status);
-
-        this.isFormValid = false;
-        this.invalidFormMessage = err.error.message;
-      }
-
-    })
+    return true;
   }
 
   navigateToLoginPage() {
